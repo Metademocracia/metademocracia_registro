fbb8f2bfbfbe1d8c1a655cda98a0f46dc93d8e8d
sebastian3500 <sebastianrosa39@gmail.com>
2023-11-25 19:34:05 -0400

    commit

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -140,7 +140,7 @@ export default{
         .then((response) => {
           console.log(response.data)
           this.loadingForm = false;
-          this.text = 'Registro creado exitósamente';
+          this.text = 'Registro creado exitosamente';
           this.snackbar = true;
         })
         .catch((error) => {
